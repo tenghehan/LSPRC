244647f3e014ea6e7093c062fa9e3d05531886bf
tenghh15 <thss15_tenghh@163.com>
2020-07-29 14:26:45 +0800

    train model and validate code

--- train.py
+++ train.py
@@ -4,22 +4,123 @@ from data.load_rap_attributes_data_mat import *
 from data.AttrDataset import AttrDataset, get_transform
 from torch.utils.data import DataLoader
 from config import argument_parser
-from utils.tools import set_seed
+from utils.tools import set_seed, get_pedestrian_metrics, AverageMeter, to_scalar
+from models.DeepMAR import DeepMAR_ResNet50
+import torch.nn.functional as F
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.nn.utils import clip_grad_norm_
+from tqdm import tqdm
 
 set_seed(100)
 
+
+def train_model(epoch, model, train_loader, valid_loader, criterion, optimizer, lr_scheduler, path):
+
+    def train(model, train_loader, criterion, optimizer):
+        model.train()
+        loss_meter = AverageMeter()
+
+        gt_list = []
+        preds_probs = []
+
+        for step, (images, gt_labels, image_filenames) in enumerate(tqdm(train_loader)):
+            images, gt_labels = images.to(device), gt_labels.to(device)
+            train_logits = model(images)
+            train_loss = criterion(train_logits, gt_labels)
+
+            train_loss.backward()
+            clip_grad_norm_(model.parameters(), max_norm=10.0)
+            optimizer.step()
+            optimizer.zero_grad()
+            loss_meter.update(to_scalar(train_loss))
+
+            gt_list.append(gt_labels.cpu().numpy())
+            train_probs = torch.sigmoid(train_logits)
+            preds_probs.append(train_probs.detach().cpu().numpy())
+
+            log_interval = 20
+            if (step + 1) % log_interval == 0 or (step + 1) % len(train_loader) == 0:
+                print(f'Step {step}/{len(train_loader)} in Ep {epoch} ',
+                      f'train_loss: {loss_meter.avg: 4f}')
+        train_loss = loss_meter.avg
+
+        gt_labels = np.concatenate(gt_list, axis=0)
+        preds_probs = np.concatenate(preds_probs, axis=0)
+
+        return train_loss, gt_labels, preds_probs
+
+    def valid(model, valid_loader, criterion):
+        model.eval()
+        loss_meter = AverageMeter()
+
+        gt_list = []
+        preds_probs = []
+
+        with torch.no_grad():
+            for step, (images, gt_labels, image_filenames) in enumerate(tqdm(valid_loader)):
+                images, gt_labels = images.to(device), gt_labels.to(device)
+                gt_list.append(gt_labels.cpu().numpy())
+                valid_logits = model(images)
+                valid_loss = criterion(valid_logits, gt_labels)
+                valid_probs = torch.sigmoid(valid_logits)
+                preds_probs.append(valid_probs.cpu().numpy())
+                loss_meter.update(to_scalar(valid_loss))
+
+        valid_loss = loss_meter.avg
+
+        gt_labels = np.concatenate(gt_list, axis=0)
+        preds_probs = np.concatenate(preds_probs, axis=0)
+        return valid_loss, gt_labels, preds_probs
+
+    maximum = float(-np.inf)
+    best_epoch = 0
+
+    result_list = []
+
+    for i in range(epoch):
+
+        train_loss, train_gt, train_probs = train(
+            model=model,
+            train_loader=train_loader,
+            criterion=criterion,
+            optimizer=optimizer,
+        )
+
+        valid_loss, valid_gt, valid_probs = valid(
+            model=model,
+            valid_loader=valid_loader,
+            criterion=criterion
+        )
+
+        lr_scheduler.step(metrics=valid_loss, epoch=i)
+
+        train_result = get_pedestrian_metrics(train_gt, train_probs)
+        valid_result = get_pedestrian_metrics(valid_gt, valid_probs)
+
+        print(f'epoch {i} \n',
+              f'training loss: {train_loss}, validate loss: {valid_loss} \n',
+              'training ma: {:.4f}, Acc: {:.4f}, Prec: {:.4f}, Rec: {:.4f}, F1: {:.4f} \n'.format(
+                  train_result.ma, train_result.instance_acc, train_result.instance_prec,
+                  train_result.instance_recall, train_result.instance_f1
+              ),
+              'validation ma: {:.4f}, Acc: {:.4f}, Prec: {:.4f}, Rec: {:.4f}, F1: {:.4f} \n'.format(
+                  valid_result.ma, valid_result.instance_acc, valid_result.instance_prec,
+                  valid_result.instance_recall, valid_result.instance_f1
+              ))
+        if valid_result.ma > maximum:
+            maximum = valid_result.ma
+            best_epoch = i
+            best_model = model
+            torch.save(best_model, path)
+
+        result_list.append((train_result, valid_result))
+
+    return maximum, best_epoch, result_list
+
+
 def main():
 
-    parser = argument_parser()
-
-    args = parser.parse_args()
-
-    use_gpu = True
-    if use_gpu:
-        torch.cuda.set_device(args.gpu_id)
-        device = torch.device(args.gpu_id)
-    else:
-        device = torch.device("cpu")
+    save_result_path = os.path.join('results', 'deepMAR_max.pth')
 
     annotation_data = loadRAPAttr(args.annotation_file)
     annotation_data = processRAPAttr(annotation_data)
@@ -61,10 +162,48 @@ def main():
         pin_memory=True
     )
 
-    for index, (images, gt_labels, image_filenames) in enumerate(valid_loader):
-        print(images.size())
-        break
+    print(f'training set: {len(train_loader.dataset)}, '
+          f'validation set: {len(valid_loader.dataset)}, '
+          f'attr_num: {len(train_dataset.attr_names_cn)}')
 
+    model = DeepMAR_ResNet50(len(train_dataset.attr_names_cn))
+    model = model.to(device)
+
+    criterion = F.binary_cross_entropy_with_logits
+    finetuned_params = []
+    new_params = []
+    for n, p in model.named_parameters():
+        if n.find('classifier') >= 0:
+            new_params.append(p)
+        else:
+            finetuned_params.append(p)
+    param_groups = [{'params': finetuned_params, 'lr': args.lr_ft},
+                    {'params': new_params, 'lr': args.lr_new}]
+    optimizer = torch.optim.SGD(param_groups, momentum=args.momentum, weight_decay=args.weight_decay, nesterov=False)
+    lr_sheduler = ReduceLROnPlateau(optimizer, factor=0.1, patience=4)
+
+    max_valid_ma, best_epoch, result_list = train_model(
+        epoch=args.epoch,
+        model=model,
+        train_loader=train_loader,
+        valid_loader=valid_loader,
+        criterion=criterion,
+        optimizer=optimizer,
+        lr_scheduler=lr_sheduler,
+        path=save_result_path
+    )
 
 if __name__ == '__main__':
+
+    parser = argument_parser()
+
+    args = parser.parse_args()
+
+    use_gpu = True
+    if use_gpu:
+        torch.cuda.set_device(args.gpu_id)
+        device = torch.device(args.gpu_id)
+    else:
+        device = torch.device("cpu")
+
     main()
--- utils/tools.py
+++ utils/tools.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 import torch
 from torchvision import transforms
+from easydict import EasyDict
 
 
 def set_seed(rand_seed):
@@ -15,3 +16,109 @@ def set_seed(rand_seed):
 def show_PILImage(tensor):
     image = transforms.ToPILImage()(tensor)
     image.show()
+
+
+def to_scalar(vt):
+    """
+    preprocess a 1-length pytorch Variable or Tensor to scalar
+    """
+    # if isinstance(vt, Variable):
+    #     return vt.data.cpu().numpy().flatten()[0]
+    if torch.is_tensor(vt):
+        if vt.dim() == 0:
+            return vt.detach().cpu().numpy().flatten().item()
+        else:
+            return vt.detach().cpu().numpy()
+    elif isinstance(vt, np.ndarray):
+        return vt
+    else:
+        raise TypeError('Input should be a ndarray or tensor')
+
+
+def get_pedestrian_metrics(gt_label, preds_probs, threshold=0.5):
+    pred_label = preds_probs > threshold
+
+    eps = 1e-20
+    result = EasyDict()
+
+    ###############################
+    # label metrics
+    # TP + FN
+    gt_pos = np.sum((gt_label == 1), axis=0).astype(float)
+    # TN + FP
+    gt_neg = np.sum((gt_label == 0), axis=0).astype(float)
+    # TP
+    true_pos = np.sum((gt_label == 1) * (pred_label == 1), axis=0).astype(float)
+    # TN
+    true_neg = np.sum((gt_label == 0) * (pred_label == 0), axis=0).astype(float)
+    # FP
+    false_pos = np.sum(((gt_label == 0) * (pred_label == 1)), axis=0).astype(float)
+    # FN
+    false_neg = np.sum(((gt_label == 1) * (pred_label == 0)), axis=0).astype(float)
+
+    label_pos_recall = 1.0 * true_pos / (gt_pos + eps)  # true positive
+    label_neg_recall = 1.0 * true_neg / (gt_neg + eps)  # true negative
+    # mean accuracy
+    label_ma = (label_pos_recall + label_neg_recall) / 2
+
+    result.label_pos_recall = label_pos_recall
+    result.label_neg_recall = label_neg_recall
+    result.label_prec = true_pos / (true_pos + false_pos + eps)
+    result.label_acc = true_pos / (true_pos + false_pos + false_neg + eps)
+    result.label_f1 = 2 * result.label_prec * result.label_pos_recall / (
+            result.label_prec + result.label_pos_recall + eps)
+
+    result.label_ma = label_ma
+    result.ma = np.mean(label_ma)
+
+    ################
+    # instance metrics
+    gt_pos = np.sum((gt_label == 1), axis=1).astype(float)
+    true_pos = np.sum((pred_label == 1), axis=1).astype(float)
+    # true positive
+    intersect_pos = np.sum((gt_label == 1) * (pred_label == 1), axis=1).astype(float)
+    # IOU
+    union_pos = np.sum(((gt_label == 1) + (pred_label == 1)), axis=1).astype(float)
+
+    instance_acc = intersect_pos / (union_pos + eps)
+    instance_prec = intersect_pos / (true_pos + eps)
+    instance_recall = intersect_pos / (gt_pos + eps)
+    instance_f1 = 2 * instance_prec * instance_recall / (instance_prec + instance_recall + eps)
+
+    instance_acc = np.mean(instance_acc)
+    instance_prec = np.mean(instance_prec)
+    instance_recall = np.mean(instance_recall)
+    instance_f1 = np.mean(instance_f1)
+
+    result.instance_acc = instance_acc
+    result.instance_prec = instance_prec
+    result.instance_recall = instance_recall
+    result.instance_f1 = instance_f1
+
+    result.error_num, result.fn_num, result.fp_num = false_pos + false_neg, false_neg, false_pos
+
+    return result
+
+class AverageMeter(object):
+    """
+    Computes and stores the average and current value
+
+    """
+
+    def __init__(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / (self.count + 1e-20)
