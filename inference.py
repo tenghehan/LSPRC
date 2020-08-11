f7dea75416c4b56652416bdf7d33acf26ab3739a
tenghh15 <thss15_tenghh@163.com>
2020-08-11 16:25:02 +0800

    output label ma

--- inference.py
+++ inference.py
@@ -6,6 +6,7 @@ from data.AttrDataset import AttrDataset, get_transform
 from torch.utils.data import DataLoader
 from models.DeepMAR import DeepMAR_ResNet50
 from tqdm import tqdm
+from utils.tools import get_pedestrian_metrics
 
 
 def load_data():
@@ -77,15 +78,17 @@ def query_results(query_list, preds_probs):
 
 def inference(model, dataloader):
     preds_probs = []
+    gt_list = []
 
     with torch.no_grad():
-        for step, (images, _, image_filenames) in enumerate(tqdm(dataloader)):
+        for step, (images, gt_labels, image_filenames) in enumerate(tqdm(dataloader)):
             images = images.to(device)
             logits = model(images)
             probs = torch.sigmoid(logits)
             preds_probs.append(probs.cpu().numpy())
+            gt_list.append(gt_labels.cpu().numpy())
 
-    return np.concatenate(preds_probs, axis=0)
+    return np.concatenate(preds_probs, axis=0), np.concatenate(gt_list, axis=0)
 
 
 def load_query(query_file_path):
@@ -98,14 +101,27 @@ def load_query(query_file_path):
     return query_list
 
 
+def output_ma_acc(preds_probs, gt_labels):
+    result = get_pedestrian_metrics(gt_labels, preds_probs)
+
+    print('label_ma:', result.label_ma)
+
+    print('vma: {:.4f}, Acc: {:.4f}, Prec: {:.4f}, Rec: {:.4f}, F1: {:.4f} \n'.format(
+              result.ma, result.instance_acc, result.instance_prec,
+              result.instance_recall, result.instance_f1))
+
+
 def main():
     dataloader = load_data()
     model = load_model()
-    preds_probs = inference(model, dataloader)
-    attributes_recognition_results(preds_probs)
+    preds_probs, gt_labels = inference(model, dataloader)
 
-    query_list = load_query('attr_query_index.txt')
-    query_results(query_list, preds_probs)
+    output_ma_acc(preds_probs, gt_labels)
+
+    # attributes_recognition_results(preds_probs)
+    #
+    # query_list = load_query('attr_query_index.txt')
+    # query_results(query_list, preds_probs)
 
 
 if __name__ == '__main__':
